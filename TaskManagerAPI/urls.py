45d3e7ee23f0cdbaadc0f2ed74292ccddfafb8a6
mikey0-1 <mikebaliscao0@gmail.com>
2026-04-11 14:22:39 +0800

    default to tasks/

--- TaskManagerAPI/urls.py
+++ TaskManagerAPI/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/tasks/', permanent=False)),
     # path('admin/', admin.site.urls),
     path('', include("taskmanager.urls")),
     path('', include("accounts.urls")),
